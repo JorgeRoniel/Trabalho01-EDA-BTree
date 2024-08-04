e3f608d1aeb6e53fd33bda7cd2bf88eca3cbe944
igorSanms <igorsantanasampaio2023@gmail.com>
2024-08-03 21:04:38 -0300

    att

--- arvoreB/Arvore.java
+++ arvoreB/Arvore.java
@@ -1,69 +1,87 @@
 package arvoreB;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Arvore {
     private static final int ordem = 3;
     private Pagina raiz;
 
-    public Arvore(){
+    public Arvore() {
         raiz = new Pagina(true);
     }
 
-    public void inserir(int valor){
+    public void inserir(int valor) {
         Pagina r = raiz;
-        if(r.getN() == 2 * ordem - 1){
+        if (r.getN() == 2 * ordem - 1) {
             Pagina aux = new Pagina(false);
             raiz = aux;
             aux.getFilhos().add(r);
             dividePag(aux, 0, r);
             inserirEmNoNaoCheio(aux, valor);
-        }else{
+        } else {
             inserirEmNoNaoCheio(r, valor);
         }
     }
 
-    private void dividePag(Pagina pai, int i, Pagina filho){
+    private void dividePag(Pagina pai, int i, Pagina filho) {
         Pagina aux = new Pagina(filho.IsFolha());
-        aux.setN(ordem-1);
+        aux.setN(ordem - 1);
 
-        for(int j = 0; j < ordem-1;j++){
+        for (int j = 0; j < ordem - 1; j++) {
             aux.getChaves().add(filho.getChaves().get(ordem));
         }
 
-        if(!filho.IsFolha()){
-            for(int j = 0; j < ordem; j++){
+        if (!filho.IsFolha()) {
+            for (int j = 0; j < ordem; j++) {
                 aux.getFilhos().add(filho.getFilhos().get(ordem));
             }
         }
 
-        filho.setN(ordem-1);
+        filho.setN(ordem - 1);
 
-        pai.getFilhos().add(i+1, aux);
-        pai.getChaves().add(i, filho.getChaves().get(ordem-1));
-        pai.setN(pai.getN()+1);
+        pai.getFilhos().add(i + 1, aux);
+        pai.getChaves().add(i, filho.getChaves().get(ordem - 1));
+        pai.setN(pai.getN() + 1);
     }
 
-    private void inserirEmNoNaoCheio(Pagina pag, int valor){
+    private void inserirEmNoNaoCheio(Pagina pag, int valor) {
         int i = pag.getN() - 1;
 
-        if(pag.IsFolha()){
+        if (pag.IsFolha()) {
             pag.getChaves().add(0);
-            while(i >= 0 && valor < pag.getChaves().get(i)){
-                pag.getChaves().set(i+1, pag.getChaves().get(i));
+            while (i >= 0 && valor < pag.getChaves().get(i)) {
+                pag.getChaves().set(i + 1, pag.getChaves().get(i));
                 i--;
             }
 
-            pag.getChaves().set(i+1, valor);
-            pag.setN(pag.getN()+1);
-        }else{
-            while(i >= 0 && valor < pag.getChaves().get(i)) i--;
+            pag.getChaves().set(i + 1, valor);
+            pag.setN(pag.getN() + 1);
+        } else {
+            while (i >= 0 && valor < pag.getChaves().get(i)) i--;
 
             i++;
-            if(pag.getFilhos().get(i).getN() == 2 * ordem - 1){
+            if (pag.getFilhos().get(i).getN() == 2 * ordem - 1) {
                 dividePag(pag, i, pag.getFilhos().get(i));
-                if(valor > pag.getChaves().get(i)) i++;
+                if (valor > pag.getChaves().get(i)) i++;
             }
             inserirEmNoNaoCheio(pag.getFilhos().get(i), valor);
         }
+    }
+
+    public int getOrdem() {
+        return ordem;
+    }
+
+    public int calcularNivel() {
+        return raiz.calcularNivel(0);
+    }
+
+    public List<Integer> coletarElementos() {
+        List<Integer> elementos = new ArrayList<>();
+        raiz.coletarElementos(elementos);
+        elementos.sort(Integer::compareTo);
+        return elementos;
     }
 
     @Override
--- arvoreB/Pagina.java
+++ arvoreB/Pagina.java
@@ -9,42 +9,64 @@ public class Pagina {
     private List<Pagina> filhos = new ArrayList<>();
     private Boolean folha;
 
-    
     public Pagina(Boolean folha) {
         this.folha = folha;
     }
 
-
     public int getN() {
         return n;
     }
+
     public void setN(int n) {
         this.n = n;
     }
+
     public List<Integer> getChaves() {
         return chaves;
     }
+
     public void setChaves(List<Integer> chaves) {
         this.chaves = chaves;
     }
+
     public List<Pagina> getFilhos() {
         return filhos;
     }
+
     public void setFilhos(List<Pagina> filhos) {
         this.filhos = filhos;
     }
+
     public Boolean IsFolha() {
         return folha;
     }
+
     public void setFolha(Boolean folha) {
         this.folha = folha;
     }
 
+    public void coletarElementos(List<Integer> elementos) {
+        elementos.addAll(chaves);
+        if (!folha) {
+            for (Pagina filho : filhos) {
+                filho.coletarElementos(elementos);
+            }
+        }
+    }
+
+    public int calcularNivel(int nivel) {
+        int maxNivel = nivel;
+        for (Pagina filho : filhos) {
+            int nivelFilho = filho.calcularNivel(nivel + 1);
+            if (nivelFilho > maxNivel) {
+                maxNivel = nivelFilho;
+            }
+        }
+        return maxNivel;
+    }
 
     @Override
     public String toString() {
         return "Pagina [chaves=" + chaves + ", filhos=" + filhos + "]";
     }
-
-    
 }
